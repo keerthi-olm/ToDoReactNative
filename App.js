4758eebe6305777ec97b33e3fb49a3767bbacd47
keerthi V <keef-vp@hotmail.com>
2018-12-24 13:33:27 +0000

    to do react native

---
+++ App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { StyleSheet, Text, View ,Button,TextInput,CheckBox} from 'react-native';
+
+export default class ToDoApp extends React.Component {
+
+    constructor(props) {
+    super();
+    this.state = { list: props.list, doneList:props.doneList };
+    this.doneList=[...props.doneList];
+    this.list=[...this.state.list];
+
+
+  }
+
+
+  render() {
+    return (
+       <View style={styles.App}>
+        <View id="list" >
+          <View className="header" ><Text lassName='title' >My to do list </Text></View>
+          <View className="add_reset_section">
+            <TextInput placeholder="Add a new task..."/>
+          </View>
+          <View className="button add">
+            <Button onClick={this._handleAddItem} title='press' >Add</Button>
+          </View>
+          <View className="button reset">
+            <Button onClick={this._handleResetList} title='press'/>
+          </View>
+            {this.state.list.map((value, i) => {
+            return <ToDoList key={i}  item={value} />;
+          })}       
+          <View className="footer"><Button className='remove'  onClick={this._handleRemoveDoneItems}  title='press' >
+          Remove
+        </Button></View>
+        </View>
+      </View>
+    );
+  }
+}
+ToDoApp.propTypes = {
+  list: PropTypes.array
+};
+
+ToDoApp.defaultProps = {
+  list: ["Get up in the morning", "Brush my teeth"],
+  doneList: []
+};
+
+class ToDoList extends React.Component {
+  constructor(props) {
+    super();
+     this.state = { value: props.item , checked:false};
+   
+  }
+  componentWillReceiveProps(nextProps) {
+    
+  }
+  _handleCheckBoxClick = (e) => {
+    
+  }
+
+  render() {
+    /** RENDER  **/
+    console.log("-- render");
+    
+    let text = this.state.checked ? <strike>{this.state.value}</strike> : this.state.value;
+    let checked= this.state.checked ? 'checked' : '';
+    return (
+      <View className="main">
+        <CheckBox  onChange={this._handleCheckBoxClick}  /><Text>{text}</Text>
+      </View>
+    );
+  }
+
+  componentWillUnmount() {
+
+  }
+}
+const styles = StyleSheet.create({
+  "App": {
+    "fontFamily": "sans-serif",
+    "textAlign": "center",
+    "fontSize": 52
+  }
+});
---
+++ convertedCss.js
@@ -0,0 +1,80 @@
+{
+  "App": {
+    "fontFamily": "sans-serif",
+    "textAlign": "center",
+    "fontSize": "1.5em"
+  },
+  "widget_ul": {
+    "padding": "0",
+    "margin": "0",
+    "listStyle": "none",
+    "height": "200px",
+    "MsBoxOrient": "horizontal",
+    "display": "flex",
+    "WebkitJustifyContent": "space-around",
+    "justifyContent": "space-around",
+    "WebkitFlexFlow": "row wrap",
+    "flexFlow": "row wrap",
+    "WebkitAlignItems": "stretch",
+    "alignItems": "stretch"
+  },
+  "header": {
+    "flex": "1 100%"
+  },
+  "footer": {
+    "flex": "1 100%"
+  },
+  "main": {
+    "flex": "1 100%"
+  },
+  "button": {
+    "color": "#fff",
+    "textTransform": "uppercase",
+    "background": "#ed3330",
+    "padding": "0.5vw",
+    "borderRadius": "5px",
+    "display": "inline-block",
+    "border": "none",
+    "height": "100%"
+  },
+  "add_reset_section": {
+    "flex": "9"
+  },
+  "widget_li": {
+    "padding": "10px",
+    "width": "500px",
+    "fontWeight": "bold",
+    "fontSize": "0.8em",
+    "textAlign": "right"
+  },
+  "widget_li_header": {
+    "textAlign": "center"
+  },
+  "widget_li_footer": {
+    "textAlign": "center"
+  },
+  "button_remove": {
+    "backgroundColor": "green",
+    "padding": "0.1em",
+    "height": ""
+  },
+  "widget_li_add_reset_section_input": {
+    "width": "100%",
+    "height": "100%"
+  },
+  "widget_li_main": {
+    "fontSize": "1.1em",
+    "lineHeight": "1.2em",
+    "display": "flex",
+    "alignItems": "center"
+  },
+  "widget__checkbox": {
+    "height": "1em",
+    "width": "1em",
+    "color": "green",
+    "marginRight": "0.2em"
+  },
+  "strike": {
+    "opacity": "0.5"
+  }
+}
